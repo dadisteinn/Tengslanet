e82f3f74c16fc211624b8cdec2a6ed9327ff3167
dadisteinn <dadisteinn@gmail.com>
2020-12-01 19:44:59 +0000

    moved db file from config to data folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const connectDB = require("./data/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
