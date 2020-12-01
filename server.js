34d85ba7fcd47c30526ac69b57561a7a5286a7d6
dadisteinn <dadisteinn@gmail.com>
2020-12-01 14:24:01 +0000

    added DB connection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server starter on port ${PORT}`));
