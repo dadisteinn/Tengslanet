aa1851f37e9ce3de265f18f7e37741e55fe39581
dadisteinn <dadisteinn@gmail.com>
2020-12-01 20:03:49 +0000

    added getUserProfile route and logic

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+const auth = require("../middleware/auth");
+const profileService = require("../services/profileService");
+
+// @route   GET api/profile/{userId}
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  profileService.getUserProfile(
+    req.user.id,
+    function (profile) {
+      return res.json(profile);
+    },
+    function (err) {
+      if (err.code) {
+        return res.status(err.code).json(err.err);
+      }
+      return res.status(500).send("Server error");
+    }
+  );
+});
 
 module.exports = router;
