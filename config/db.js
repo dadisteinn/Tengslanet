90bce7b5e94a4235534aaa01dd0259181f4681c7
dadisteinn <dadisteinn@gmail.com>
2020-12-01 16:55:35 +0000

    added dotenv and .env file

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log("Successfully connected to MongoDB");
   } catch (err) {
