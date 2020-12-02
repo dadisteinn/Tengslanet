28dc2e3d265ac2084799c6dd6586e159f2c1da6a
dadisteinn <dadisteinn@gmail.com>
2020-12-02 14:37:47 +0000

    added a few profile routes

--- routes/profile.js
+++ routes/profile.js
@@ -1,25 +1,98 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 const auth = require("../middleware/auth");
 const profileService = require("../services/profileService");
 
-// @route   GET api/profile/{userId}
+// @route   GET api/profile/me
 // @desc    Get current users profile
 // @access  Private
 router.get("/me", auth, async (req, res) => {
-  profileService.getUserProfile(
-    req.user.id,
-    function (profile) {
+  try {
+    const profile = await profileService.getUserProfile(req.user.id);
+    return res.json(profile);
+  } catch (err) {
+    if (err.code) {
+      return res.status(err.code).json(err.err);
+    }
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const profile = await profileService.createOrUpdateProfile(
+        req.body,
+        req.user.id
+      );
       return res.json(profile);
-    },
-    function (err) {
+    } catch (err) {
       if (err.code) {
         return res.status(err.code).json(err.err);
       }
       return res.status(500).send("Server error");
     }
-  );
+  }
+);
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await profileService.getAllProfiles();
+    return res.json(profiles);
+  } catch (err) {
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route   GET api/profile/user/:userId
+// @desc    Get profile by user id
+// @access  Public
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const profiles = await profileService.getProfileByUserId(req.params.userId);
+    return res.json(profiles);
+  } catch (err) {
+    if (err.code) {
+      return res.status(err.code).json(err.err);
+    }
+    if (err.kind) {
+      return res.status(400).send("Profile not found");
+    }
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    await profileService.DeleteProfileById(req.user.id);
+    return res.status(204).json({ msg: "User deleted" });
+  } catch (err) {
+    return res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
--- services/profileService.js
+++ services/profileService.js
@@ -2,22 +2,121 @@ const Profile = require("../models/Profile");
 const User = require("../models/User");
 
 const profileService = () => {
-  const getUserProfile = async (userId, successCb, errorCb) => {
+  const getProfileFields = (body, userId) => {
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = body;
+
+    // Create profile obj
+    const profileFields = {};
+    profileFields.user = userId;
+
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // Add social media
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+
+    return profileFields;
+  };
+
+  const getUserProfile = async (userId) => {
     const profile = await Profile.findOne({ user: userId }).populate("user", [
       "name",
       "avatar",
     ]);
     if (!profile) {
-      errorCb({
+      throw {
         code: 400,
         err: { errors: [{ msg: "No profile was found for this user" }] },
-      });
+      };
     }
-    successCb(profile);
+    return profile;
+  };
+
+  const createProfile = async (body, userId) => {
+    const profileFields = getProfileFields(body, userId);
+
+    // Find profile based on user id
+    let profile = await Profile.findOne({ user: userId });
+
+    // Update profile or a create new one if none was found
+    if (profile) {
+      profile = await Profile.findOneAndUpdate(
+        { user: userId },
+        { $set: profileFields },
+        {
+          new: true,
+          upsert: true,
+          setDefaultsOnInsert: true,
+        }
+      );
+    } else {
+      profile = new Profile(profileFields);
+      await profile.save();
+    }
+    return profile;
+  };
+
+  const getAllProfiles = async () => {
+    // Find all profiles
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    return profiles;
+  };
+
+  const getProfileByUserId = async (userId) => {
+    const profile = await Profile.findOne({ user: userId }).populate("user", [
+      "name",
+      "avatar",
+    ]);
+    if (!profile) {
+      throw {
+        code: 400,
+        err: { errors: [{ msg: "Profile not found" }] },
+      };
+    }
+    return profile;
+  };
+
+  const DeleteProfileById = async (userId) => {
+    // TODO: Remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: userId });
+    // Remove user
+    await User.findOneAndRemove({ _id: userId });
+    // TODO: blacklist token?
   };
 
   return {
     getUserProfile,
+    createProfile,
+    getAllProfiles,
+    getProfileByUserId,
+    DeleteProfileById,
   };
 };
 
