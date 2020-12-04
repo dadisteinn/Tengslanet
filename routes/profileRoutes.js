156844f5f0e934c22b6b7fc6cd70d9253396afc2
dadisteinn <dadisteinn@gmail.com>
2020-12-04 10:13:12 +0000

    added 'middleware' to middleware filenames  moved 404 page to error.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { check, validationResult } from "express-validator";
-import auth from "../middleware/auth.js";
+import auth from "../middleware/authMiddleware.js";
 import profileService from "../services/profileService.js";
 import { ServerError } from "../errors.js";
 
